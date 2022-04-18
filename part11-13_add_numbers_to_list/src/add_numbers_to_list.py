550c54df7beb3b4d773cf5a990c4a1e5006fc3bb
Stephen Brink <stephenb.k101010@null.net>
2022-04-18 14:34:50 -0600

    completed section

--- part11-13_add_numbers_to_list/src/add_numbers_to_list.py
+++ part11-13_add_numbers_to_list/src/add_numbers_to_list.py
@@ -1,1 +1,4 @@
-# WRITE YOUR SOLUTION HERE:
+def add_numbers_to_list(numbers: list):
+    if len(numbers)%5 != 0:
+        numbers.append(numbers[-1]+1)
+        add_numbers_to_list(numbers)
--- part11-15_balanced_brackets/src/balanced_brackets.py
+++ part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,8 +1,8 @@
-
 def balanced_brackets(my_string: str):
+    my_string = ''.join([character for character in my_string if character in '()[]'])
     if len(my_string) == 0:
         return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
+    if not (my_string[0] == '(' and my_string[-1] == ')') and not (my_string[0] == '[' and my_string[-1] == ']'):
         return False
 
     # remove first and last character
