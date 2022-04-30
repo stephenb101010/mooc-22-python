53b99d85a9568596205dc1762d1ed36daeec4a26
Stephen Brink <stephenb.k101010@null.net>
2022-04-30 02:37:42 -0600

    completed two. update third.

--- part13-04_random_robots/src/main.py
+++ part13-04_random_robots/src/main.py
@@ -1,1 +1,28 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+import random
+
+pygame.init()
+
+window = pygame.display.set_mode((640, 480))
+robot = pygame.image.load("robot.png")
+width = robot.get_width()
+height = robot.get_height()
+window.fill((0,0,0))
+
+coords_list = []
+
+for i in range (0, 1001):
+    x = random.randint(0, 800 - width)
+    y = random.randint(0, 800 - height)
+    coords_list.append((x, y))
+
+
+for tuple in coords_list:
+    window.blit(robot, (tuple[0], tuple[1]))
+pygame.display.flip()
+
+   
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
