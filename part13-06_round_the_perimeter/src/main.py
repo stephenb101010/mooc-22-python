c42e3187e98365f01d28410a6de4d67e4972078e
Stephen Brink <stephenb.k101010@null.net>
2022-05-05 23:32:04 -0600

    completed more assignments

--- part13-06_round_the_perimeter/src/main.py
+++ part13-06_round_the_perimeter/src/main.py
@@ -8,6 +8,8 @@ robot = pygame.image.load("robot.png")
 x = 0
 y = 0
 velocity = 1
+h = 1
+v = 0
 clock = pygame.time.Clock()
 
 while True:
@@ -19,11 +21,30 @@ while True:
     window.blit(robot, (x, y))
     pygame.display.flip()
 
-#Need to make a loop where it moves right along x, hits edge, moves down along y, then reverse    
-    y += velocity
-    if velocity > 0 and y + robot.get_height() >= 480:
-        velocity = -velocity
-    if velocity < 0 and y <= 0:
-        velocity = -velocity
-
+    if h == 1 and v == 0:
+        x += velocity
+        y = 0
+    elif h == 0 and v == 1:
+        x = 640-robot.get_width()
+        y += velocity
+    elif h == -1 and v == 0:
+        x += -velocity
+        y = 480-robot.get_height()
+    elif h == 0 and v == -1:
+        x = 0
+        y += -velocity
+    
+    if h == 1 and v == 0 and x+robot.get_width() >= 640:
+        h = 0
+        v = 1
+    if h == 0 and v == 1 and y+robot.get_height() >= 480:
+        h = -1
+        v = 0
+    if h == -1 and v == 0 and x <= 0:
+        h = 0
+        v = -1
+    if h == 0 and v == -1 and y <= 0:
+        h = 1
+        v = 0
+    
     clock.tick(60)
